cc3eaee4db05b73f92dfb9c10f6dd09b91007ec0
Md-Rakib-Hassan <md.rakibhassan4591@gmail.com>
2024-10-24 13:02:40 +0600

    add patch route for gps

--- index.js
+++ index.js
@@ -1,14 +1,55 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port=process.env.PORT || 5000;
+require('dotenv').config();
+const port = process.env.PORT || 5000;
 app.use(express.json());
-
-app.listen(port, ()=>{
-    console.log('server is running');
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.uztm9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
-app.post('/gps',async function (req, res) {
-    const gps_info=req.body;
-    console.log(gps_info);
-    res.send(gps_info);
-  })
+console.log(process.env);
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    const dataBase = client.db("DIU_Connect");
+
+    app.post("/gps", async function (req, res) {
+      const Collection = dataBase.collection("gps");
+      const gps_info = req.body;
+      console.log(gps_info);
+      res.send(gps_info);
+    });
+
+    app.patch("/gps", async (req, res) => {
+      const { latitude, longitude } = req.body;
+      const objectId = new ObjectId("6719eca6761ba4c222624894");
+      const result = await db.collection("gps").updateOne({ _id: objectId }, { $set: { latitude, longitude } });
+
+        if (result.matchedCount === 0) {
+          return res.status(404);
+        }
+        res.status(200);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log("server is running");
+});
